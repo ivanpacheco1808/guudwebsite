382da87722f347a4a642a3263c90638d5e4f10e9
Miguel Angel <m.duran@garsalabs.com>
2023-03-31 16:13:21 -0600

    Mejoras en el motor de animaciones: - Tiempos de respuesta - Administracion de animaciones - Prevencion de repeticiones - Adaptacion a mobil

--- src/app/guudcomponents/individuales/imageplacer/imageplacer.component.ts
+++ src/app/guudcomponents/individuales/imageplacer/imageplacer.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
-import {KeyValue} from '@angular/common';
+import { KeyValue } from '@angular/common';
 
-declare function SVGInject(data):any;
+declare function SVGInject(data): any;
 
 @Component({
   selector: 'guud-imageplacer',
@@ -10,53 +10,104 @@ declare function SVGInject(data):any;
 })
 export class ImageplacerComponent {
   constructor() { }
-  _imageList:any;
-  _centerDisplay:any;
-  _mobilekeys:any;
-  @Input() set mobilekeys(list:any){this._mobilekeys=list;}
-  @Input() set imageList(list:any){
-    this._imageList=list;
-    this._centerDisplay = [...list.grupocentral.files, ...list.grupomysc.files];
+  _imageList: any;
+  _mobilekeys: any;
+  _rotation: any = {
+    web: []
+  };
+  _fadecolors = ["fadeToRed", "fadeToBlue", "fadeToGreen"];
+  @Input() set mobilekeys(list: any) { this._mobilekeys = list; }
+  @Input() set imageList(list: any) {
+    //Parametros Iniciales
+    this._imageList = list;
+
+    //MainView
+    for (let key of this._mobilekeys) {
+      for (let data of list[key].files) {
+        if (data.alt != 'phrase0') {
+          this._rotation.web.push(data.alt);
+        }
+      }
+    };
+    for (let data of list.grupomysc.files) {
+      if (data.alt != 'phrase0') {
+        this._rotation.web.push(data.alt);
+      }
+    }
+    this._rotation.web.push('phrase0');
+
+    //Animaciones
     setTimeout(() => {
-      this._startAnimation();
+      //Anima primer icono
+      var rindx = Math.floor((Math.random() * 6) / 2);
+      var inicialweb: any = document.querySelector("[id='webphrase0']");
+      $(inicialweb).addClass(this._fadecolors[rindx]);
+      setTimeout(() => {
+        $(inicialweb).removeClass(this._fadecolors[rindx]);
+      }, 3500);
+      this._startMobAnimation();
+      this._startWebAnimation();
     }, 777);
   }
-  _checktype(data:any, filename:string){
-    if(filename.indexOf('.svg')>=0){
+
+  _startWebAnimation() {
+    setInterval(() => {
+      var randfade = Math.floor((Math.random() * 6) / 2);
+      var rindx = Math.floor(Math.random() * (this._rotation.web.length - 2));
+      var ref: any = document.querySelector("[id='web" + this._rotation.web[rindx] + "']");
+      this._rotation.web.push(this._rotation.web[rindx]);
+      this._rotation.web.splice(rindx, 1);
+
+      $(ref).addClass(this._fadecolors[randfade]);
+      setTimeout(() => {
+        $(ref).removeClass(this._fadecolors[randfade]);
+      }, 3500);
+
+    }, 3000);
+  }
+
+  _switchgroup(group:string) {
+    var rindx = Math.floor(Math.random() * (this._imageList[group].files.length - 2));
+    var lastalt = this._imageList[group].files[this._imageList[group].files.length-1].alt;
+    var lastref: any = document.querySelector("[id='mob" + lastalt + "']");
+    var nextalt = this._imageList[group].files[rindx].alt;
+    var nextref: any = document.querySelector("[id='mob" + nextalt + "']");
+
+    var randfade = Math.floor((Math.random() * 6) / 2);
+    $(lastref).addClass(this._fadecolors[randfade]);
+    setTimeout(() => {
+      $(lastref).removeClass(this._fadecolors[randfade]);
+    }, 3500);
+    setTimeout(() => {
+      $(lastref).removeClass('ondisplay');
+      setTimeout(() => {
+        $(nextref).addClass('ondisplay');
+      }, 500);
+    }, 4000);
+
+    /*
+      demo.push(demo[3]);
+      demo.splice(3, 1);
+      console.log(demo);
+    */
+    this._imageList[group].files.push(this._imageList[group].files[rindx]);
+    this._imageList[group].files.splice(rindx, 1);
+    this._mobilekeys.push(this._mobilekeys[this._mobilekeys.indexOf(group)]);
+    this._mobilekeys.splice(this._mobilekeys.indexOf(group), 1);
+  }
+
+  _startMobAnimation() {
+    this._switchgroup('grupod');
+    setInterval(()=> {
+      var selectedcat = this._mobilekeys[Math.floor(Math.random() * (this._mobilekeys.length-2))];
+      this._switchgroup(selectedcat);
+    }, 3000);
+  }
+
+  _checktype(data: any, filename: string) {
+    if (filename.indexOf('.svg') >= 0) {
       SVGInject(data.srcElement);
     }
   }
 
-  _startAnimation() {
-    //Ignora logos
-    var logo1: any = document.querySelector("[id='logo']");
-    var logo2: any = document.querySelector("[id='logobottom']");
-    $(logo1).addClass("nohover");
-    $(logo2).addClass("nohover");
-    //Anima primer icono
-    var inicial: any = document.querySelector("[id='webphrase0']");
-    $(inicial).addClass("fadeToBlue");
-    setTimeout(() => {
-      $(inicial).removeClass("fadeToBlue");
-    }, 3500);
-    //Anima primer icono
-    var inicial: any = document.querySelector("[id='mobphrase0']");
-    $(inicial).addClass("fadeToBlue");
-    setTimeout(() => {
-      $(inicial).removeClass("fadeToBlue");
-    }, 3500);
-
-    setInterval(() => {
-      var randfade = Math.floor(Math.random() * 6) / 2;
-      var fadecolors = ["fadeToRed", "fadeToBlue", "fadeToGreen"];
-      var nextAnimation = Math.floor(Math.random() * 22);
-      var ref: any = document.querySelector("[id='webphrase" + nextAnimation + "']");
-      //if (!$(ref).hasClass(fadecolors[randfade])) {
-        $(ref).addClass(fadecolors[randfade]);
-        setTimeout(() => {
-          $(ref).removeClass(fadecolors[randfade]);
-        }, 3500);
-      //}
-    }, 3000);
-  }
 }
--- src/app/views/mainview/mainview.component.ts
+++ src/app/views/mainview/mainview.component.ts
@@ -13,74 +13,74 @@ export class MainviewComponent {
   constructor() { }
 
   mobilekeys = ['grupoa','grupob','grupoc','grupod','grupoe','grupof'];
-  phrases = {
+  phrasesMainView = {
     //GRUPOS DE VISION MOBILE - GRUPOS DE VISION MOBILE - GRUPOS DE VISION MOBILE
     grupocentral:{
       files:[
-        { filename: 'logo-Guud.png', webStyle: { left: '32vw', top: '20.2vw', width: '34.5vw' }, mobileStyle: { left: '5vw', top: '82.4vw', width: '91vw' }, alt: 'logo', onDisplay:true },
-        { filename: 'Frase-23.svg', webStyle: { left: '38.9vw', top: '28vw', width: '20.4vw' }, mobileStyle: { left: '8vw', top: '102vw', width: '84vw' }, alt: 'logobottom', onDisplay:true },
+        { filename: 'logo-Guud.png', webStyle: { left: '32vw', top: '20.2vw', width: '34.5vw' }, mobileStyle: { left: '5vw', top: '82.4vw', width: '91vw' }, alt: 'logo' },
+        { filename: 'Frase-23.svg', webStyle: { left: '38.9vw', top: '28vw', width: '20.4vw' }, mobileStyle: { left: '8vw', top: '102vw', width: '84vw' }, alt: 'logobottom' },
       ]
     },
     grupomysc:{
       files:[
         //  NO MOBILE NO MOBILE
-        { filename: 'Frase-9.svg', webStyle: { left: '64.3vw', top: '31.4vw', width: '12vw' }, mobileStyle: { left: '60vw', top: '183vw', width: '12vw' }, alt: 'phrase9', onDisplay:false },
-        { filename: 'Frase-13.svg', webStyle: { left: '76.2vw', top: '18.4vw', width: '11.7vw' }, mobileStyle: { left: '86vw', top: '188vw', width: '12vw' }, alt: 'phrase13', onDisplay:false },
-        { filename: 'flechas/Flecha-1.svg', webStyle: { left: '1.6vw', top: '34.3vw', width: '8.9vw' }, mobileStyle: { left: '2vw', top: '172vw', width: '9vw' }, alt: 'arrow1', onDisplay:false },
-        { filename: 'flechas/Flecha-2.svg', webStyle: { left: '26.2vw', top: '42.9vw', width: '8.6vw' }, mobileStyle: { left: '2vw', top: '172vw', width: '9vw' }, alt: 'arrow2', onDisplay:false },
-        { filename: 'flechas/Flecha-3.svg', webStyle: { left: '66.2vw', top: '43.2vw', width: '8vw' }, mobileStyle: { left: '2vw', top: '172vw', width: '9vw' }, alt: 'arrow3', onDisplay:false },
-        { filename: 'flechas/Flecha-4.svg', webStyle: { left: '94.2vw', top: '22.5vw', width: '3.4vw' }, mobileStyle: { left: '2vw', top: '172vw', width: '9vw' }, alt: 'arrow4', onDisplay:false },
-        { filename: 'flechas/Flecha-5.svg', webStyle: { left: '69.7vw', top: '10.4vw', width: '9.2vw' }, mobileStyle: { left: '2vw', top: '172vw', width: '9vw' }, alt: 'arrow5', onDisplay:false },
+        { filename: 'Frase-9.svg', webStyle: { left: '64.3vw', top: '31.4vw', width: '12vw' }, mobileStyle: { left: '60vw', top: '183vw', width: '12vw' }, alt: 'phrase9' },
+        { filename: 'Frase-13.svg', webStyle: { left: '76.2vw', top: '18.4vw', width: '11.7vw' }, mobileStyle: { left: '86vw', top: '188vw', width: '12vw' }, alt: 'phrase13' },
+        { filename: 'flechas/Flecha-1.svg', webStyle: { left: '1.6vw', top: '34.3vw', width: '8.9vw' }, mobileStyle: { left: '2vw', top: '172vw', width: '9vw' }, alt: 'arrow1' },
+        { filename: 'flechas/Flecha-2.svg', webStyle: { left: '26.2vw', top: '42.9vw', width: '8.6vw' }, mobileStyle: { left: '2vw', top: '172vw', width: '9vw' }, alt: 'arrow2' },
+        { filename: 'flechas/Flecha-3.svg', webStyle: { left: '66.2vw', top: '43.2vw', width: '8vw' }, mobileStyle: { left: '2vw', top: '172vw', width: '9vw' }, alt: 'arrow3' },
+        { filename: 'flechas/Flecha-4.svg', webStyle: { left: '94.2vw', top: '22.5vw', width: '3.4vw' }, mobileStyle: { left: '2vw', top: '172vw', width: '9vw' }, alt: 'arrow4' },
+        { filename: 'flechas/Flecha-5.svg', webStyle: { left: '69.7vw', top: '10.4vw', width: '9.2vw' }, mobileStyle: { left: '2vw', top: '172vw', width: '9vw' }, alt: 'arrow5' },
       ]
     },
     grupoa:{
       files:[
         //GRUPO A - GRUPO A - GRUPO A - GRUPO A - GRUPO A - GRUPO A - GRUPO A - GRUPO A
-        { filename: 'Frase-8.svg', webStyle: { left: '46.2vw', top: '40.6vw', width: '14.8vw' }, mobileStyle: { left: '5vw', top: '25vw', width: '55vw' }, alt: 'phrase8', onDisplay:true },
-        { filename: 'Frase-1.svg', webStyle: { left: '6vw', top: '4.3vw', width: '17.7vw' }, mobileStyle: { left: '8vw', top: '20.6vw', width: '52vw' }, alt: 'phrase1', onDisplay:false },
-        { filename: 'Frase-15.svg', webStyle: { left: '74.9vw', top: '12.1vw', width: '9vw' }, mobileStyle: { left: '9vw', top: '20vw', width: '48vw' }, alt: 'phrase15', onDisplay:false },
+        { filename: 'Frase-1.svg', webStyle: { left: '6vw', top: '4.3vw', width: '17.7vw' }, mobileStyle: { left: '8vw', top: '20.6vw', width: '52vw' }, alt: 'phrase1' },
+        { filename: 'Frase-15.svg', webStyle: { left: '74.9vw', top: '12.1vw', width: '9vw' }, mobileStyle: { left: '9vw', top: '20vw', width: '48vw' }, alt: 'phrase15' },
+        { filename: 'Frase-8.svg', webStyle: { left: '46.2vw', top: '40.6vw', width: '14.8vw' }, mobileStyle: { left: '5vw', top: '25vw', width: '55vw' }, alt: 'phrase8' },
       ]
     },
     grupob:{
       files:[
         //GRUPO B - GRUPO B - GRUPO B - GRUPO B - GRUPO B - GRUPO B - GRUPO B - GRUPO B
-        { filename: 'Frase-20.svg', webStyle: { left: '49vw', top: '32.2vw', width: '11.4vw' }, mobileStyle: { left: '53.7vw', top: '36.5vw', width: '44vw' }, alt: 'phrase20', onDisplay:true },
-        { filename: 'Frase-2.svg', webStyle: { left: '0.6vw', top: '10.1vw', width: '10.9vw' }, mobileStyle: { left: '60vw', top: '22vw', width: '33vw' }, alt: 'phrase2', onDisplay:false },
-        { filename: 'Frase-17.svg', webStyle: { left: '64.6vw', top: '4.2vw', width: '10.9vw' }, mobileStyle: { left: '58.7vw', top: '36vw', width: '32vw' }, alt: 'phrase17', onDisplay:false },
+        { filename: 'Frase-2.svg', webStyle: { left: '0.6vw', top: '10.1vw', width: '10.9vw' }, mobileStyle: { left: '60vw', top: '22vw', width: '33vw' }, alt: 'phrase2' },
+        { filename: 'Frase-17.svg', webStyle: { left: '64.6vw', top: '4.2vw', width: '10.9vw' }, mobileStyle: { left: '58.7vw', top: '36vw', width: '32vw' }, alt: 'phrase17' },
+        { filename: 'Frase-20.svg', webStyle: { left: '49vw', top: '32.2vw', width: '11.4vw' }, mobileStyle: { left: '53.7vw', top: '36.5vw', width: '44vw' }, alt: 'phrase20' },
       ]
     },
     grupoc:{
       files:[
         //GRUPO C - GRUPO C - GRUPO C - GRUPO C - GRUPO C - GRUPO C - GRUPO C - GRUPO C
-        { filename: 'Frase-6.svg', webStyle: { left: '1.7vw', top: '25.6vw', width: '10.5vw' }, mobileStyle: { left: '15vw', top: '50vw', width: '29vw' }, alt: 'phrase6', onDisplay:true },
-        { filename: 'Frase-10.svg', webStyle: { left: '78.1vw', top: '42vw', width: '17.6vw' }, mobileStyle: { left: '5vw', top: '59.7vw', width: '58.6vw' }, alt: 'phrase10', onDisplay:false },
-        { filename: 'Frase-11.svg', webStyle: { left: '79.6vw', top: '37.2vw', width: '15.2vw' }, mobileStyle: { left: '13vw', top: '59vw', width: '59vw' }, alt: 'phrase11', onDisplay:false },
-        { filename: 'Frase-18.svg', webStyle: { left: '56.2vw', top: '10.4vw', width: '12.9vw' }, mobileStyle: { left: '7vw', top: '56vw', width: '50vw' }, alt: 'phrase18', onDisplay:false },
+        { filename: 'Frase-10.svg', webStyle: { left: '78.1vw', top: '42vw', width: '17.6vw' }, mobileStyle: { left: '5vw', top: '59.7vw', width: '58.6vw' }, alt: 'phrase10' },
+        { filename: 'Frase-11.svg', webStyle: { left: '79.6vw', top: '37.2vw', width: '15.2vw' }, mobileStyle: { left: '13vw', top: '59vw', width: '59vw' }, alt: 'phrase11' },
+        { filename: 'Frase-18.svg', webStyle: { left: '56.2vw', top: '10.4vw', width: '12.9vw' }, mobileStyle: { left: '7vw', top: '56vw', width: '50vw' }, alt: 'phrase18' },
+        { filename: 'Frase-6.svg', webStyle: { left: '1.7vw', top: '25.6vw', width: '10.5vw' }, mobileStyle: { left: '15vw', top: '50vw', width: '29vw' }, alt: 'phrase6' },
       ]
     },
     grupod:{
       files:[
         //GRUPO D - GRUPO D - GRUPO D - GRUPO D - GRUPO D - GRUPO D - GRUPO D - GRUPO D
-        { filename: 'Frase-22.svg', webStyle: { left: '15vw', top: '25.8vw', width: '9.8vw' }, mobileStyle: { left: '7vw', top: '116vw', width: '32vw' }, alt: 'phrase0', onDisplay:true },
-        { filename: 'Frase-4.svg', webStyle: { left: '21.5vw', top: '4.2vw', width: '16.9vw' }, mobileStyle: { left: '9vw', top: '114vw', width: '40vw' }, alt: 'phrase4', onDisplay:false },
-        { filename: 'Frase-16.svg', webStyle: { left: '81.8vw', top: '4.1vw', width: '15.5vw' }, mobileStyle: { left: '8vw', top: '118vw', width: '50vw' }, alt: 'phrase16', onDisplay:false },
+        { filename: 'Frase-4.svg', webStyle: { left: '21.5vw', top: '4.2vw', width: '16.9vw' }, mobileStyle: { left: '9vw', top: '114vw', width: '40vw' }, alt: 'phrase4' },
+        { filename: 'Frase-16.svg', webStyle: { left: '81.8vw', top: '4.1vw', width: '15.5vw' }, mobileStyle: { left: '8vw', top: '118vw', width: '50vw' }, alt: 'phrase16' },
+        { filename: 'Frase-22.svg', webStyle: { left: '15vw', top: '25.8vw', width: '9.8vw' }, mobileStyle: { left: '7vw', top: '116vw', width: '32vw' }, alt: 'phrase0' },
       ]
     },
     grupoe:{
       files:[
         //GRUPO E - GRUPO E - GRUPO E - GRUPO E - GRUPO E - GRUPO E - GRUPO E - GRUPO E
-        { filename: 'Frase-7.svg', webStyle: { left: '0.1vw', top: '36.4vw', width: '26.6vw' }, mobileStyle: { left: '10vw', top: '143vw', width: '84vw' }, alt: 'phrase7', onDisplay:true },
-        { filename: 'Frase-5.svg', webStyle: { left: '35.2vw', top: '4.6vw', width: '20.6vw' }, mobileStyle: { left: '17vw', top: '149vw', width: '67vw' }, alt: 'phrase5', onDisplay:false },
-        { filename: 'Frase-12.svg', webStyle: { left: '79.9vw', top: '28.3vw', width: '17.2vw' }, mobileStyle: { left: '16vw', top: '144vw', width: '68vw' }, alt: 'phrase12', onDisplay:false },
-        { filename: 'Frase-14.svg', webStyle: { left: '88.7vw', top: '12.4vw', width: '9.5vw' }, mobileStyle: { left: '34vw', top: '145vw', width: '32vw' }, alt: 'phrase14', onDisplay:false },
+        { filename: 'Frase-5.svg', webStyle: { left: '35.2vw', top: '4.6vw', width: '20.6vw' }, mobileStyle: { left: '17vw', top: '149vw', width: '67vw' }, alt: 'phrase5' },
+        { filename: 'Frase-12.svg', webStyle: { left: '79.9vw', top: '28.3vw', width: '17.2vw' }, mobileStyle: { left: '16vw', top: '144vw', width: '68vw' }, alt: 'phrase12' },
+        { filename: 'Frase-14.svg', webStyle: { left: '88.7vw', top: '12.4vw', width: '9.5vw' }, mobileStyle: { left: '34vw', top: '145vw', width: '32vw' }, alt: 'phrase14' },
+        { filename: 'Frase-7.svg', webStyle: { left: '0.1vw', top: '36.4vw', width: '26.6vw' }, mobileStyle: { left: '10vw', top: '143vw', width: '84vw' }, alt: 'phrase7' },
       ]
     },
     grupof:{
       files:[
         //GRUPO F - GRUPO F - GRUPO F - GRUPO F - GRUPO F - GRUPO F - GRUPO F - GRUPO F
-        { filename: 'Frase-3.svg', webStyle: { left: '11.6vw', top: '14.1vw', width: '10.9vw' }, mobileStyle: { left: '53vw', top: '114vw', width: '36vw' }, alt: 'phrase3', onDisplay:true },
-        { filename: 'Frase-19.svg', webStyle: { left: '56.2vw', top: '4.2vw', width: '5.4vw' }, mobileStyle: { left: '60.6vw', top: '119.4vw', width: '20vw' }, alt: 'phrase19', onDisplay:false },
-        { filename: 'Frase-21.svg', webStyle: { left: '30.7vw', top: '34.7vw', width: '13.3vw' }, mobileStyle: { left: '49vw', top: '116vw', width: '41vw' }, alt: 'phrase21', onDisplay:false },
+        { filename: 'Frase-19.svg', webStyle: { left: '56.2vw', top: '4.2vw', width: '5.4vw' }, mobileStyle: { left: '60.6vw', top: '119.4vw', width: '20vw' }, alt: 'phrase19' },
+        { filename: 'Frase-21.svg', webStyle: { left: '30.7vw', top: '34.7vw', width: '13.3vw' }, mobileStyle: { left: '49vw', top: '116vw', width: '41vw' }, alt: 'phrase21' },
+        { filename: 'Frase-3.svg', webStyle: { left: '11.6vw', top: '14.1vw', width: '10.9vw' }, mobileStyle: { left: '53vw', top: '114vw', width: '36vw' }, alt: 'phrase3' },
       ]
     }
   };
