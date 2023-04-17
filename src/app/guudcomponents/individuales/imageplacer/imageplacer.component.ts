1c8b47c8bfdf76961fff26cff2c8a4de1ec124d4
Miguel Angel <m.duran@garsalabs.com>
2023-04-17 13:04:12 -0600

    Mejoras despues de la revision de Google

--- src/app/guudcomponents/individuales/imageplacer/imageplacer.component.ts
+++ src/app/guudcomponents/individuales/imageplacer/imageplacer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { KeyValue } from '@angular/common';
 
 declare function SVGInject(data): any;
@@ -8,20 +8,35 @@ declare function SVGInject(data): any;
   templateUrl: './imageplacer.component.html',
   styleUrls: ['./imageplacer.component.scss']
 })
-export class ImageplacerComponent implements OnDestroy {
+export class ImageplacerComponent implements OnDestroy, OnInit {
+  _rotation: any = {
+    web: ['phrase9', 'phrase13', 'arrow1', 'arrow2', 'arrow3', 'arrow4', 'arrow5', 'phrase1', 'phrase15', 'phrase8', 'phrase2', 'phrase17', 'phrase20', 'phrase10', 'phrase11', 'phrase18', 'phrase6', 'phrase4', 'phrase16', 'phrase0', 'phrase5', 'phrase12', 'phrase14', 'phrase7', 'phrase19', 'phrase21', 'phrase3']
+  };
+  _fadecolors = ["fadeToRed", "fadeToBlue", "fadeToGreen"];
+  ngOnInit(){
+
+    //Animaciones
+    setTimeout(() => {
+      //Anima primer icono
+      var rindx = Math.floor((Math.random() * 6) / 2);
+      var inicialweb: any = document.querySelector("[id='webphrase0']");
+      $(inicialweb).addClass(this._fadecolors[rindx]);
+
+      setTimeout(() => {
+        $(inicialweb).removeClass(this._fadecolors[rindx]);
+      }, 3500);
+      this._startMobAnimation();
+      this._startWebAnimation();
+    }, 777);
+  }
+
   ngOnDestroy() {
     clearInterval(this._webInterval)
     clearInterval(this._mobInterval)
   }
   _webInterval:any;
   _mobInterval:any;
-  constructor() { }
-  _imageList: any;
-  _mobilekeys: any;
-  _rotation: any = {
-    web: []
-  };
-  _fadecolors = ["fadeToRed", "fadeToBlue", "fadeToGreen"];
+  constructor() {}
   @Input() set mobilekeys(list: any) { this._mobilekeys = list; }
   @Input() set imageList(list: any) {
     //Parametros Iniciales
@@ -41,7 +56,7 @@ export class ImageplacerComponent implements OnDestroy {
       }
     }
     this._rotation.web.push('phrase0');
-
+/*
     //Animaciones
     setTimeout(() => {
       //Anima primer icono
@@ -54,6 +69,7 @@ export class ImageplacerComponent implements OnDestroy {
       this._startMobAnimation();
       this._startWebAnimation();
     }, 777);
+*/
   }
 
   _startWebAnimation() {
@@ -115,5 +131,80 @@ export class ImageplacerComponent implements OnDestroy {
       SVGInject(data.srcElement);
     }
   }
+
+
+  _mobilekeys = ['grupoa','grupob','grupoc','grupod','grupoe','grupof'];
+  _imageList = {
+    //GRUPOS DE VISION MOBILE - GRUPOS DE VISION MOBILE - GRUPOS DE VISION MOBILE
+    grupocentral:{
+      files:[
+        { filename: 'logo-Guud.png', alt: 'logo' },
+        { filename: 'Frase-23.svg', alt: 'logobottom' },
+      ]
+    },
+    grupomysc:{
+      files:[
+        //  NO MOBILE NO MOBILE
+        { filename: 'Frase-9.svg', alt: 'phrase9' },
+        { filename: 'Frase-13.svg', alt: 'phrase13' },
+        { filename: 'flechas/Flecha-1.svg', alt: 'arrow1' },
+        { filename: 'flechas/Flecha-2.svg', alt: 'arrow2' },
+        { filename: 'flechas/Flecha-3.svg', alt: 'arrow3' },
+        { filename: 'flechas/Flecha-4.svg', alt: 'arrow4' },
+        { filename: 'flechas/Flecha-5.svg', alt: 'arrow5' },
+      ]
+    },
+
+    grupoa:{
+      files:[
+        //GRUPO A - GRUPO A - GRUPO A - GRUPO A - GRUPO A - GRUPO A - GRUPO A - GRUPO A
+        { filename: 'Frase-1.svg', alt: 'phrase1' },
+        { filename: 'Frase-15.svg', alt: 'phrase15' },
+        { filename: 'Frase-8.svg', alt: 'phrase8' },
+      ]
+    },
+    grupob:{
+      files:[
+        //GRUPO B - GRUPO B - GRUPO B - GRUPO B - GRUPO B - GRUPO B - GRUPO B - GRUPO B
+        { filename: 'Frase-2.svg', alt: 'phrase2' },
+        { filename: 'Frase-17.svg', alt: 'phrase17' },
+        { filename: 'Frase-20.svg', alt: 'phrase20' },
+      ]
+    },
+    grupoc:{
+      files:[
+        //GRUPO C - GRUPO C - GRUPO C - GRUPO C - GRUPO C - GRUPO C - GRUPO C - GRUPO C
+        { filename: 'Frase-10.svg', alt: 'phrase10' },
+        { filename: 'Frase-11.svg', alt: 'phrase11' },
+        { filename: 'Frase-18.svg', alt: 'phrase18' },
+        { filename: 'Frase-6.svg', alt: 'phrase6' },
+      ]
+    },
+    grupod:{
+      files:[
+        //GRUPO D - GRUPO D - GRUPO D - GRUPO D - GRUPO D - GRUPO D - GRUPO D - GRUPO D
+        { filename: 'Frase-4.svg', alt: 'phrase4' },
+        { filename: 'Frase-16.svg', alt: 'phrase16' },
+        { filename: 'Frase-22.svg', alt: 'phrase0' },
+      ]
+    },
+    grupoe:{
+      files:[
+        //GRUPO E - GRUPO E - GRUPO E - GRUPO E - GRUPO E - GRUPO E - GRUPO E - GRUPO E
+        { filename: 'Frase-5.svg', alt: 'phrase5' },
+        { filename: 'Frase-12.svg', alt: 'phrase12' },
+        { filename: 'Frase-14.svg', alt: 'phrase14' },
+        { filename: 'Frase-7.svg', alt: 'phrase7' },
+      ]
+    },
+    grupof:{
+      files:[
+        //GRUPO F - GRUPO F - GRUPO F - GRUPO F - GRUPO F - GRUPO F - GRUPO F - GRUPO F
+        { filename: 'Frase-19.svg', alt: 'phrase19' },
+        { filename: 'Frase-21.svg', alt: 'phrase21' },
+        { filename: 'Frase-3.svg', alt: 'phrase3' },
+      ]
+    }
+  };
 
 }
