54fe2b155b5f3441ce79747e7b1ba0859684b514
Miguel Angel <m.duran@garsalabs.com>
2023-06-05 09:16:25 -0600

    Reduccion de video

--- src/app/views/history/history.component.ts
+++ src/app/views/history/history.component.ts
@@ -1,12 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'guudview-history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.scss']
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit {
   constructor() { }
+  ngOnInit(){
+    setTimeout(() => {
+      (<any>document.querySelector('#videoplayerhistory')).play();
+    }, 777);
+  }
   _apply:boolean=true;
   @Input() set _applyshadow(apply:boolean){this._apply=apply;}
 }
