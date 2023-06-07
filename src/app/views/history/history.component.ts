02a128778607a5348c2ca7d27aa385ad558ea43e
Miguel Angel <m.duran@garsalabs.com>
2023-06-07 10:51:04 -0600

    Cambio de video por url de streaming

--- src/app/views/history/history.component.ts
+++ src/app/views/history/history.component.ts
@@ -1,17 +1,36 @@
-import { Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit, OnDestroy, HostListener } from '@angular/core';
 
 @Component({
   selector: 'guudview-history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.scss']
 })
-export class HistoryComponent implements OnInit {
+export class HistoryComponent implements OnInit, OnDestroy {
   constructor() { }
-  ngOnInit(){
-    setTimeout(() => {
-      (<any>document.querySelector('#videoplayerhistory')).play();
-    }, 777);
+  innerWidth: any;
+  innerHeight: any;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+    this.innerHeight = window.innerHeight;
   }
-  _apply:boolean=true;
-  @Input() set _applyshadow(apply:boolean){this._apply=apply;}
+
+  public myPlayer;
+  ngOnDestroy(){
+    this.myPlayer.dispose();
+  }
+  ngOnInit() {
+    this.myPlayer = amp('videoplayerhistory', {
+      autoplay: true,
+      controls: true
+    });
+    this.myPlayer.src({
+      type: "application/vnd.apple.mpegurl",
+      src: "https://guudvideo2-usea.streaming.media.azure.net/88ba8898-f0c7-48eb-b138-bdf267589411/HistoriaGUUD.ism/manifest(format=m3u8-cmaf)"
+    });
+
+  }
+  _apply: boolean = true;
+  @Input() set _applyshadow(apply: boolean) { this._apply = apply; }
 }
