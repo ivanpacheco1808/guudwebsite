09a9b994a91d46db501b08942e7cb5d8474feed9
Miguel Angel <m.duran@garsalabs.com>
2023-08-22 12:51:09 -0600

    analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'guud';
+
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig = {
+    apiKey: "AIzaSyAyULh1nMD8IT4ThefHws0GfzotiHdu8qk",
+    authDomain: "guudlanding.firebaseapp.com",
+    projectId: "guudlanding",
+    storageBucket: "guudlanding.appspot.com",
+    messagingSenderId: "103877281107",
+    appId: "1:103877281107:web:fcd65e3527c17ad6498da6",
+    measurementId: "G-LFCQPTB2L0"
+  };
+
+  // Initialize Firebase
+  app = initializeApp(this.firebaseConfig);
+  analytics = getAnalytics(this.app);
 }
