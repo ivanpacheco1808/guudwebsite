f1c5dd287013c7eb72410d7af8758d438e3ca582
Miguel Angel <m.duran@garsalabs.com>
2023-12-05 08:33:43 -0600

    Observaciones Santos Noviembre - Ajustes grid - Cambio de formularios a 2 columnas en web - Cambios de textos - Custom videoplayer - Grid de frases

--- src/app/views/supplier/supplier.component.ts
+++ src/app/views/supplier/supplier.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'guudview-supplier',
   templateUrl: './supplier.component.html',
   styleUrls: ['./supplier.component.scss']
 })
-export class SupplierComponent {
-  constructor() { }
+export class SupplierComponent implements OnInit {
+  routerEvents: any;
+  currentRoute:string;
+  isMobile:boolean = false;
+  constructor(private router: Router) {
+    this.isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    this.routerEvents = this.router.events.subscribe(
+      (event:any)=>{
+        if(event instanceof NavigationEnd){
+          this.currentRoute=event.url;
+        }
+      }
+    )
+  }
+  ngOnInit(): void {
+  }
+  ngOnDestroy(): void {
+    this.routerEvents.unsubscribe();
+    // Unsubscribe to avoid memory leak
+  }
+
 }
