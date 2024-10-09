2ffe321ff74fa1944e8e29b122fb4467f8fe2451
_44413_ <m.duran@garsalabs.com>
2024-10-09 09:35:34 -0600

    version control type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,6 +38,7 @@ export class AppComponent {
   currentRoute:string;
   isMobile:boolean = false;
   constructor(private router: Router) {
+    console.log('github');
     this.isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     this.routerEvents = this.router.events.subscribe(
       (event:any)=>{
